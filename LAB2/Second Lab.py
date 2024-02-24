1d986c9b5ca2e01039971e6f5a83dcec9c9a8c2e
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-02-24 20:26:22 +0200

    add method for quick sort

---
+++ LAB2/Second Lab.py
@@ -0,0 +1,16 @@
+import time
+import matplotlib.pyplot as plt
+from decimal import Decimal, Context, ROUND_HALF_EVEN
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[len(arr) // 2]
+        left = [x for x in arr if x < pivot]
+        middle = [x for x in arr if x == pivot]
+        right = [x for x in arr if x > pivot]
+        return quick_sort(left) + middle + quick_sort(right)
+
+
+
