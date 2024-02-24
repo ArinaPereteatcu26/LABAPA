41983d522506f768f70630fcd057718005a5bbc5
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-02-24 20:28:57 +0200

    add method quick sort

--- LAB2/Second Lab.py
+++ LAB2/Second Lab.py
@@ -2,6 +2,7 @@ import time
 import matplotlib.pyplot as plt
 from decimal import Decimal, Context, ROUND_HALF_EVEN
 
+#quick sort
 def quick_sort(arr):
     if len(arr) <= 1:
         return arr
@@ -12,5 +13,38 @@ def quick_sort(arr):
         right = [x for x in arr if x > pivot]
         return quick_sort(left) + middle + quick_sort(right)
 
+#merge sort
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2  # Finding the mid of the array
+        L = arr[:mid]  # Dividing the array elements into 2 halves
+        R = arr[mid:]
 
+        merge_sort(L)  # Sorting the first half
+        merge_sort(R)  # Sorting the second half
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+    return arr
 
