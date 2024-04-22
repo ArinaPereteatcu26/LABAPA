17b23e5e154f9e2e4056fd73b087f4947615e7fb
Arina Pereteatcu <arinapereteatcu0@gmail.com>
2024-04-22 18:27:41 +0300

    add class algorithms

--- Lab5/Algorithms.py
+++ Lab5/Algorithms.py
@@ -9,3 +9,17 @@ class Graph:
         if u not in self.graph:
             self.graph[u] = {}
         self.graph[u][v] = w
+
+
+class Algorithms:
+    def kruskal(self, graph):
+        result = []
+        graph_edges = self.get_edges(graph)
+        graph_edges.sort(key=lambda x: x[2])  # Sort edges by weight
+        parent = {node: node for node in graph}
+
+        for u, v, w in graph_edges:
+            if self.find(parent, u) != self.find(parent, v):
+                result.append([u, v, w])
+                self.union(parent, u, v)
+                return result
